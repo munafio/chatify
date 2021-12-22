ba2a05947a83562d8cf24717d833ae71c2622524
munafio <munafaqeelmahdi@gmail.com>
2021-12-23 00:30:51 +0300

    fix migration's rollback, prefix added.

--- src/database/migrations/2019_10_16_211433_create_favorites_table.php
+++ src/database/migrations/2019_10_16_211433_create_favorites_table.php
@@ -30,6 +30,6 @@ class CreateFavoritesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('favorites');
+        Schema::dropIfExists('ch_favorites');
     }
 }
