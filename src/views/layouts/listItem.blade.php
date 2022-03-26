70f4b1315831b36473accdf39ee48218dd263d6c
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-03-26 13:47:02 +0600

    fix: bookmark icon center

--- src/views/layouts/listItem.blade.php
+++ src/views/layouts/listItem.blade.php
@@ -4,7 +4,7 @@
         <tr data-action="0">
             {{-- Avatar side --}}
             <td>
-            <div class="avatar av-m" style="background-color: #d9efff; text-align: center;">
+            <div class="avatar av-m" style="background-color: #d9efff; text-align: center; display: flex; flex-direction: column; align-items: center; justify-content: center;">
                 <span class="far fa-bookmark" style="font-size: 22px; color: #68a5ff;"></span>
             </div>
             </td>
