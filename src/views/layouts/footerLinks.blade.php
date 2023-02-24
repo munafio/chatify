d8147a9f501e73fc50576fbdc7253d2f77664ac2
Munaf A. Mahdi <munafaqeelmahdi@gmail.com>
2023-02-25 01:34:09 +0300

    pusher js updated to  v7.0.0

--- src/views/layouts/footerLinks.blade.php
+++ src/views/layouts/footerLinks.blade.php
@@ -1,4 +1,4 @@
-<script src="https://js.pusher.com/7.0.3/pusher.min.js"></script>
+<script src="https://js.pusher.com/7.2.0/pusher.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@joeattardi/emoji-button@3.0.3/dist/index.min.js"></script>
 <script >
   // Enable pusher logging - don't include this in production
