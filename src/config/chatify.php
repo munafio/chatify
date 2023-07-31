b936ef918b3ddcbf0f25b4746874f4f3fd9f6830
Saurav Kumal <sk.sauravkumal@gmail.com>
2023-08-01 00:03:25 +0545

    revert personalized formatting

--- src/config/chatify.php
+++ src/config/chatify.php
@@ -29,7 +29,7 @@ return [
     */
     'routes' => [
         'prefix' => env('CHATIFY_ROUTES_PREFIX', 'chatify'),
-        'middleware' => env('CHATIFY_ROUTES_MIDDLEWARE', ['web', 'auth']),
+        'middleware' => env('CHATIFY_ROUTES_MIDDLEWARE', ['web','auth']),
         'namespace' => env('CHATIFY_ROUTES_NAMESPACE', 'Chatify\Http\Controllers'),
     ],
     'api_routes' => [
@@ -50,7 +50,7 @@ return [
         'app_id' => env('PUSHER_APP_ID'),
         'options' => [
             'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
-            'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'mt1') . '.pusher.com',
+            'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
             'port' => env('PUSHER_PORT', 443),
             'scheme' => env('PUSHER_SCHEME', 'https'),
             'encrypted' => true,
@@ -90,8 +90,8 @@ return [
     'attachments' => [
         'folder' => 'attachments',
         'download_route_name' => 'attachments.download',
-        'allowed_images' => (array)['png', 'jpg', 'jpeg', 'gif'],
-        'allowed_files' => (array)['zip', 'rar', 'txt'],
+        'allowed_images' => (array) ['png','jpg','jpeg','gif'],
+        'allowed_files' => (array) ['zip','rar','txt'],
         'max_upload_size' => env('CHATIFY_MAX_FILE_SIZE', 150), // MB
     ],
 
@@ -100,7 +100,7 @@ return [
     | Messenger's colors
     |-------------------------------------
     */
-    'colors' => (array)[
+    'colors' => (array) [
         '#2180f3',
         '#2196F3',
         '#00BCD4',
