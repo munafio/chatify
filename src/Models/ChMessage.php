50061edd50070b159e578235a1d1df4a12290ff4
Tarik Manoar <tarikmanoar@gmail.com>
2024-04-06 09:05:07 +0600

    Add new trait HasMessage for checking read and unread message

--- src/Models/ChMessage.php
+++ src/Models/ChMessage.php
@@ -8,4 +8,90 @@ use Chatify\Traits\UUID;
 class ChMessage extends Model
 {
     use UUID;
+
+    /**
+     * Get the user who sent the message.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function from()
+    {
+        return $this->belongsTo(User::class, 'from_id');
+    }
+
+    /**
+     * Get the user who received the message.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function to()
+    {
+        return $this->belongsTo(User::class, 'to_id');
+    }
+
+    /**
+     * Mark the notification as read.
+     *
+     * @return void
+     */
+    public function markAsRead()
+    {
+        if ($this->seen !== 1) {
+            $this->forceFill(['seen' => 1])->save();
+        }
+    }
+
+    /**
+     * Mark the notification as unread.
+     *
+     * @return void
+     */
+    public function markAsUnread()
+    {
+        if ($this->seen !== 0) {
+            $this->forceFill(['seen' => 0])->save();
+        }
+    }
+
+    /**
+     * Determine if a notification has been read.
+     *
+     * @return bool
+     */
+    public function read()
+    {
+        return $this->seen !== 0;
+    }
+
+    /**
+     * Determine if a notification has not been read.
+     *
+     * @return bool
+     */
+    public function unread()
+    {
+        return $this->seen === 0;
+    }
+
+    /**
+     * Scope a query to only include read notifications.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeRead(Builder $query)
+    {
+        return $query->where('seen', 1);
+    }
+
+    /**
+     * Scope a query to only include unread notifications.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeUnread(Builder $query)
+    {
+        return $query->where('seen', 0);
+    }
 }
